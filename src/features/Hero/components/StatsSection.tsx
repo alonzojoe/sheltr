b68e3f00849e317687f0c14b2113ddbd5f7a63dc
alonzojoe <alonzojoenell2k15@gmail.com>
2025-09-23 11:40:51 +0800

    create new StatItem component reaveive props title and subTitle

--- src/features/Hero/components/StatsSection.tsx
+++ src/features/Hero/components/StatsSection.tsx
@@ -1,5 +1,26 @@
+import Wrapper from "@/components/Containers/Wrapper";
 const StatsSection = () => {
-  return <div>Stats Section</div>;
+  return (
+    <section id="stats">
+      <Wrapper className="px-3 py-2 mt-20 grid items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        <StatItem title={`340+`} subTitle="Clients helped in their search " />
+      </Wrapper>
+    </section>
+  );
+};
+
+type StatItemProps = {
+  title: string;
+  subTitle: string;
+};
+
+const StatItem = ({ title, subTitle }: StatItemProps) => {
+  return (
+    <div className="flex items-center flex-col space-y-4">
+      <h2 className="font-bold text-2xl md:text-3xl text-text-dark">{title}</h2>
+      <p className="text-base text-text-sub">{subTitle}</p>
+    </div>
+  );
 };
 
 export default StatsSection;
