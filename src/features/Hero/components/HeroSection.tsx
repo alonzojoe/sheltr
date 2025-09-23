5918ce5fc58999fce6757dc6bdeb9602af078c9b
alonzojoe <alonzojoenell2k15@gmail.com>
2025-09-23 11:07:33 +0800

    add responsiveness > HeroSection

--- src/features/Hero/components/HeroSection.tsx
+++ src/features/Hero/components/HeroSection.tsx
@@ -6,21 +6,21 @@ import { FaStar, FaArrowRight } from "react-icons/fa6";
 const HeroSection = () => {
   return (
     <section id="home" className="pt-20 lg:pt-32">
-      <Wrapper className="px-0 py-2 bg-accent grid items-center grid-cols-1 md:grid-cols-2 gap-8">
-        <div className="flex flex-col space-y-7 pl-10">
-          <h1 className="text-title text-text-dark font-bold leading-16 whitespace-normal">
+      <Wrapper className="px-0 pl-0 md:pl-10 py-2 bg-accent grid items-center grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="flex flex-col space-y-7 order-2 md:order-1">
+          <h1 className="text-center mt-10 md:mt-0 md:text-left text-4xl md:text-title text-text-dark font-bold md:leading-16 whitespace-normal">
             Your home to find, our comfort achieved
           </h1>
-          <p className="text-text-sub">
+          <p className="text-text-sub text-center md:text-left text-base md:text-body">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt
           </p>
-          <div className="flex items-center gap-7">
+          <div className="flex flex-col md:flex-row items-center gap-7">
             <FeatureStar value={5} />
             <FeatureStar value={4} />
             <FeatureStar value={4} />
           </div>
-          <div className="flex items-center gap-5">
+          <div className="flex justify-center md:justify-start items-center gap-5 mb-10">
             <Button className="bg-primary text-white text-base py-3 px-4 hover:opacity-95">
               Envision your next home
             </Button>
@@ -29,7 +29,7 @@ const HeroSection = () => {
             </Button>
           </div>
         </div>
-        <div className="flex items-center justify-end relative">
+        <div className="flex items-center justify-end relative order-1 md:order-2">
           <img
             className="h-auto w-[553px] xl:w-[753px]"
             src={HeroImg}
