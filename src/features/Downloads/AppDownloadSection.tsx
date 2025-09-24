5c2884d1a128cbf71317765f311469f2347b82c8
alonzojoe <alonzojoenell2k15@gmail.com>
2025-09-24 14:48:03 +0800

    create new AppDownloadSection add component base layout with Wrapper Container and bg color as accent

---
+++ src/features/Downloads/AppDownloadSection.tsx
@@ -0,0 +1,11 @@
+import Wrapper from "@/components/Containers/Wrapper";
+
+const AppDownloadSection = () => {
+  return (
+    <section id="download">
+      <Wrapper className="py-2 px-3 mt-30 bg-accent">App Download</Wrapper>
+    </section>
+  );
+};
+
+export default AppDownloadSection;
