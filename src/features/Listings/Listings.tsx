a01fad82c5f9127eba442421e4256c742ef21fe6
alonzojoe <alonzojoenell2k15@gmail.com>
2025-09-26 10:13:22 +0800

    create new component Listings implement base class and container, implement in Index Page.

---
+++ src/features/Listings/Listings.tsx
@@ -0,0 +1,11 @@
+import Wrapper from "@/components/Containers/Wrapper";
+
+const Listings = () => {
+  return (
+    <div>
+    <Wrapper className="py-2 px-3i">Listings</Wrapper>
+    </div>
+  );
+};
+
+export default Listings;
