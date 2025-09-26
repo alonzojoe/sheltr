bc5c39dbb81330da683249be4be0671f20ea46df
alonzojoe <alonzojoenell2k15@gmail.com>
2025-09-26 10:39:04 +0800

    create new SectionHeader component define props types and implement in LisitngSection

--- src/features/Listings/ListingsSection.tsx
+++ src/features/Listings/ListingsSection.tsx
@@ -1,9 +1,14 @@
 import Wrapper from "@/components/Containers/Wrapper";
-
+import SectionHeader from "@/components/shared/SectionHeader";
 const ListingsSection = () => {
   return (
     <div>
-      <Wrapper className="py-2 px-3i">Listings</Wrapper>
+      <Wrapper className="py-2 px-3 mt-30">
+        <SectionHeader
+          title={`Based on your location`}
+          subTitle={`Start discover your liking future home`}
+        />
+      </Wrapper>
     </div>
   );
 };
