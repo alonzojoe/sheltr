602b05044a1e5cb3e8e03df067fc5998665217f1
alonzojoe <alonzojoenell2k15@gmail.com>
2025-09-30 09:45:02 +0800

    implement useQuery in ListingsSection integrate fetchRentals api call (type-safed)

--- src/features/Listings/ListingsSection.tsx
+++ src/features/Listings/ListingsSection.tsx
@@ -1,7 +1,17 @@
 import Wrapper from "@/components/Containers/Wrapper";
 import SectionHeader from "@/components/shared/SectionHeader";
 import Switcher from "./components/Switcher";
+import { useQuery } from "@tanstack/react-query";
+import { fetchRentals } from "@/services/api-calls/rentals";
+
 const ListingsSection = () => {
+  const { data } = useQuery({
+    queryKey: ["rentals"],
+    queryFn: fetchRentals,
+  });
+
+  console.log("rentals", data);
+
   return (
     <div>
       <Wrapper className="py-2 px-3 mt-30">
