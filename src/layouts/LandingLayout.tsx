674aa3c5041a879794ce0496e7a955e073f6e349
alonzojoe <alonzojoenell2k15@gmail.com>
2025-10-02 10:23:13 +0800

    create new Footer component and implement in Landing Layout

--- src/layouts/LandingLayout.tsx
+++ src/layouts/LandingLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "@/layouts/landing/components/Navbar";
-
+import Footer from "@/layouts/landing/components/Footer";
 const LandingLayout = () => {
   return (
     <>
@@ -8,6 +8,7 @@ const LandingLayout = () => {
       <main className="text-body text-sub antialiased">
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 };
---
+++ src/layouts/landing/components/Footer.tsx
@@ -0,0 +1,6 @@
+import Wrapper from "@/components/Containers/Wrapper";
+const Footer = () => {
+  return <Wrapper className="px-3 py-2 bg-red-300">Footer Component</Wrapper>;
+};
+
+export default Footer;
