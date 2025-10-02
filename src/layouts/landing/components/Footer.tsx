f584d882787d34ddb63c806f43f7bf3ef5d1bc6f
alonzojoe <alonzojoenell2k15@gmail.com>
2025-10-02 11:36:43 +0800

    Footer right section, links

--- src/layouts/landing/components/Footer.tsx
+++ src/layouts/landing/components/Footer.tsx
@@ -6,12 +6,12 @@ import { MdLocalPhone, MdMail } from "react-icons/md";
 const Footer = () => {
   return (
     <Wrapper className="px-3 py-2 border-t border-gray-300 mt-30">
-      <div className="pt-10 flex-between flex-wrap gap-10">
+      <div className="pt-10 flex justify-between flex-wrap gap-10">
         <div className="space-y-7">
           <img src={Logo} alt="shelter-logo" height={28} width={130} />
           <p className="text-text-dark text-sm">
-            Got a question or concern? Feel free to reach out to us anytime via
-            email or phone—we’re here to help!
+            Feel free to reach out to us anytime via email or phone—we’re here
+            to help!
           </p>
           <div className="flex items-center gap-2 text-sm text-text-sub">
             <MdMail className="text-lg" />
@@ -21,6 +21,80 @@ const Footer = () => {
             <MdLocalPhone className="text-lg" />
             <span>(888) 6548-8562</span>
           </div>
+        </div>
+        <div className="flex justify-between flex-wrap gap-10">
+          <ul className="flex flex-col space-y-4">
+            <li>
+              <span className="font-bold text-sm">Support</span>
+            </li>
+            <li className="text-text-sub text-sm">
+              <a href="#">Help Center</a>
+            </li>
+            <li className="text-text-sub text-sm">
+              <a href="#">Get help with a safety issue</a>
+            </li>
+            <li className="text-text-sub text-sm">
+              <a href="#">AirCover</a>
+            </li>
+            <li className="text-text-sub text-sm">
+              <a href="#">Anti-discrimination</a>
+            </li>
+            <li className="text-text-sub text-sm">
+              <a href="#">Disability support</a>
+            </li>
+            <li className="text-text-sub text-sm">
+              <a href="#">Cancellation options</a>
+            </li>
+            <li className="text-text-sub text-sm">
+              <a href="#">Report neighborhood concern</a>
+            </li>
+          </ul>
+          <ul className="flex flex-col space-y-4">
+            <li>
+              <span className="font-bold text-sm">Hosting</span>
+            </li>
+            <li className="text-text-sub text-sm">
+              <a href="#">Sheltr your home</a>
+            </li>
+            <li className="text-text-sub text-sm">
+              <a href="#">Sheltr your experience</a>
+            </li>
+            <li className="text-text-sub text-sm">
+              <a href="#">Sheltr your service</a>
+            </li>
+            <li className="text-text-sub text-sm">
+              <a href="#">Hosting resources</a>
+            </li>
+            <li className="text-text-sub text-sm">
+              <a href="#">Community forum</a>
+            </li>
+            <li className="text-text-sub text-sm">
+              <a href="#">Hosting responsibly</a>
+            </li>
+          </ul>
+          <ul className="flex flex-col space-y-4">
+            <li>
+              <span className="font-bold text-sm">Sheltr</span>
+            </li>
+            <li className="text-text-sub text-sm">
+              <a href="#">2025 Summer Release</a>
+            </li>
+            <li className="text-text-sub text-sm">
+              <a href="#">Newsroom</a>
+            </li>
+            <li className="text-text-sub text-sm">
+              <a href="#">Careers</a>
+            </li>
+            <li className="text-text-sub text-sm">
+              <a href="#">Investors</a>
+            </li>
+            <li className="text-text-sub text-sm">
+              <a href="#">Gift cards</a>
+            </li>
+            <li className="text-text-sub text-sm">
+              <a href="#">Sheltr emergency stays</a>
+            </li>
+          </ul>
         </div>
       </div>
     </Wrapper>
