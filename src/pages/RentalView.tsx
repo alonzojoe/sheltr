5a84e04075f955900cc1860bda3d9ee464f1bb54
alonzojoe <alonzojoenell2k15@gmail.com>
2025-10-09 16:54:32 +0800

    implement useParams to get the slug type-safed. implement viewRentals using useQuery indicate key and fn pass the slug as arg in the viewRental api call

--- src/pages/RentalView.tsx
+++ src/pages/RentalView.tsx
@@ -1,8 +1,26 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { viewRental } from "@/services/api-calls/rentals";
 import Wrapper from "@/components/Containers/Wrapper";
+import { Navigate } from "react-router-dom";
+
+type Params = {
+  slug: string;
+};
+
 const RentalView = () => {
+  const { slug } = useParams<Params>();
+
+  if (!slug) return <Navigate to="/" />;
+
+  const { data, isFetching, error } = useQuery({
+    queryKey: ["rentals", slug],
+    queryFn: () => viewRental(slug),
+  });
+
   return (
     <div className="pt-20 lg:pt-32 ">
-      <Wrapper className="px-3 py-2"></Wrapper>
+      <Wrapper className="px-3 py-2">{slug}</Wrapper>
     </div>
   );
 };
