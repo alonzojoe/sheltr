fc8b9ffff1e20dd09331550104a8d15c195bbba5
alonzojoe <alonzojoenell2k15@gmail.com>
2025-10-09 16:29:29 +0800

    create new services api call filter by slug return single object of rental item.

--- src/pages/RentalView.tsx
+++ src/pages/RentalView.tsx
@@ -1,5 +1,10 @@
+import Wrapper from "@/components/Containers/Wrapper";
 const RentalView = () => {
-  return <div className="pt-20 lg:pt-32">Rental View</div>;
+  return (
+    <div className="pt-20 lg:pt-32 ">
+      <Wrapper className="px-3 py-2"></Wrapper>
+    </div>
+  );
 };
 
 export default RentalView;
